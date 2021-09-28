127f9508fc2027cd599dab86f503d8b6ab90f75a
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-09-28 20:43:10 -0300

    Prod

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: "http://localhost:8080",
+  //baseUrl: "http://localhost:8080",
+baseUrl:" https://curso-spring-booot.herokuapp.com/",
   bucketBaseUrl: "https://projeto-spring.s3.sa-east-1.amazonaws.com"
 }
